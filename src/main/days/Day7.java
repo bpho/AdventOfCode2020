6dc88f911cb8d2107fed3cee025c452b08c10b28
Benny Ho <ho.bennyp@gmail.com>
2021-01-10 14:39:41 -0500

    Finishing day 7

--- src/main/days/Day7.java
+++ src/main/days/Day7.java
@@ -2,25 +2,81 @@ package main.days;
 
 import util.Common;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
 
-/*
-Create HashMap of bag types, and for each bag type have a list
-of other bag colors that it's able to hold
-
-Part 1 - Don't worry about the number of each bag type it can hold
-Focus on the list of bags after the "contain" keyword
- */
 public class Day7 {
 
+    /*
+    Part 1:
+    Recursively check bags that are able to hold "shiny gold" -- storing result in a Set
+    to avoid duplicate bags
+    Example: If dark blue bag (can hold) -> faded green -> shiny gold AND dim lavender -> faded green,
+    do not count faded green twice
+     */
+    private Set<String> determineBagsThatCanHold(List<String> inputLines, String bagColor) {
+        Set<String> totalBagColors = new HashSet<>();
+        List<String> recursiveBags = new ArrayList<>();
+        for (String line : inputLines) {
+            String[] stringSplit = line.split("contain");
+            if (stringSplit[1].contains(bagColor)) {
+                recursiveBags.add(line);
+                System.out.println("Contents for " +stringSplit[0] +": " +stringSplit[1]);
+                totalBagColors.add(stringSplit[0]);
+            }
+        }
+        if (!recursiveBags.isEmpty()) {
+            System.out.println("----- NEXT SET -----");
+            for (String line : recursiveBags) {
+                // Gather new list of bags that can hold shiny gold, construct new bagColor string to check
+                String[] bagColorRecursiveSplit = line.split("contain")[0].split(" ");
+                String bagColorRecursive = String.join(" ", bagColorRecursiveSplit[0], bagColorRecursiveSplit[1]);
+                totalBagColors.addAll(determineBagsThatCanHold(inputLines, bagColorRecursive));
+            }
+        }
+        return totalBagColors;
+    }
+
+    /*
+    Part 2:
+    Recursively check each bag that shiny gold can hold
+     */
+    private int determineBagsContainedWithin(List<String> inputLines, String bagColor, int numOfBags) {
+        int totalNumOfBags = 0;
+        for (String line : inputLines) {
+            String[] stringSplit = line.split("contain");
+            if (stringSplit[0].contains(bagColor)) {
+                String[] listOfContainingBags = Arrays.stream(stringSplit[1].split(","))
+                        .map(String::trim)
+                        .toArray(String[]::new);
+                for (String containingBag : listOfContainingBags) {
+                    if (containingBag.contains("no other bags")) {
+                        // Return 0 if there are no other bags (will happen within a subsequent call)
+                        return totalNumOfBags;
+                    } else {
+                        String[] bagText = containingBag.split(" ");
+                        int subNumOfBags = Integer.parseInt(bagText[0]);
+                        String subBagColor = String.join(" ", bagText[1], bagText[2]);
+                        // Add num of bags from current set from total, then recursively add the num of bags
+                        // from each subsequent set of bag totals
+                        totalNumOfBags += subNumOfBags;
+                        totalNumOfBags += determineBagsContainedWithin(inputLines, subBagColor, subNumOfBags);
+                    }
+                }
+            }
+        }
+        totalNumOfBags = numOfBags * totalNumOfBags;
+        return totalNumOfBags;
+    }
+
     public static void main(String[] args) {
-        String filePath = "src/resources/day6_input.txt";
+        String filePath = "src/resources/day7_input.txt";
         Day7 day7 = new Day7();
         List<String> inputLines = Common.buildList(filePath);
+        String bagColor = "shiny gold";
+        int numOfBagColors = day7.determineBagsThatCanHold(inputLines, bagColor).size();
+        System.out.println("Part 1 - Number of bags that can hold " +bagColor +": "+numOfBagColors);
+        int totalNumOfBags = day7.determineBagsContainedWithin(inputLines, bagColor, 1);
+        System.out.println("Part 2 - Number of bags that shiny gold can hold: " +totalNumOfBags);
     }
 
 }
